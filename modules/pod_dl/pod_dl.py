1e2a20b114770639f28ecc144c012934c06e9736
Eric Ransom <6969787+EricRa@users.noreply.github.com>
2024-02-14 21:16:23 -0500

    ruff check and format

--- modules/pod_dl/pod_dl.py
+++ modules/pod_dl/pod_dl.py
@@ -17,41 +17,45 @@ titles without actually downloading anything.
 import sys
 import argparse
 
-
 import feedparser
 import requests
 
+
 def dl_with_requests(link, filename):
     r2 = requests.get(link)
-    #print(type(r2.content))
-    #print(sys.getsizeof(r2.content))
+    # print(type(r2.content))
+    # print(sys.getsizeof(r2.content))
     with open(filename, "wb") as f:
         f.write(r2.content)
-        #print(sys.getsizeof(f))
+        # print(sys.getsizeof(f))
+
 
 def pod_dl(url, titles):
-
     # Check to see if RSS URL can be accessed and, if so, get the feed
     # using requests.  If not, return the http error code and end the program.
     try:
         r = requests.get(url)
     except Exception as e:
-        print(f"There was an error: {e}.\n\nPlease ensure that a valid URL"
-          " was provided.")
+        print(
+            f"There was an error: {e}.\n\nPlease ensure that a valid URL"
+            " was provided."
+        )
         sys.exit()
-        
+
     status = r.status_code
     if status == 200:
         pass
     else:
-        print("There was a problem accessing the RSS feed.  HTTP Status "
-          f"code {status} was returned.  Please ensure that the RSS URL"
-          " provided is accessable from a web browser.")
+        print(
+            "There was a problem accessing the RSS feed.  HTTP Status "
+            f"code {status} was returned.  Please ensure that the RSS URL"
+            " provided is accessable from a web browser."
+        )
         sys.exit()
-        
+
     # Parse the feed with feedparser
     d = feedparser.parse(r.text)
-    
+
     # check to see if titles argument is True.  If so, print a list of titles
     # and end the program
     if titles is True:
@@ -60,41 +64,40 @@ def pod_dl(url, titles):
         sys.exit()
     else:
         pass
-    
+
     # Download all episodes
-    
+
     for item in d.entries:
         for link in item.enclosures:
             pod_url = link.get("href")
-            print(pod_url)
+            #print(pod_url)
             filename = item.title + ".mp3"
             filename = filename.replace(":", "-")
             filename = filename.replace(" ", "_")
             filename = filename.replace("/", "_")
             filename = filename.replace("?", "_")
-            #print(filename)
+            # print(filename)
             print(f"Now downloading: {filename}")
-            dl_with_requests(pod_url,filename)
-    
+            dl_with_requests(pod_url, filename)
+
+
 # argument parser
 
 parser = argparse.ArgumentParser(
     prog="podcast downloader",
     description="A password generator using the Python secrets module",
-    epilog=""
+    epilog="",
 )
-parser.add_argument(
-    "-u",
-    "--url",
-    help="URL of the RSS feed"
-)
+parser.add_argument("-u", "--url", help="URL of the RSS feed")
 parser.add_argument(
     "-t",
     "--titles",
     action="store_true",
     default=False,
-    help=("Will attempt to return the titles of all podcast episodes "
-      "in the feed without actually downloading the episodes")
+    help=(
+        "Will attempt to return the titles of all podcast episodes "
+        "in the feed without actually downloading the episodes"
+    ),
 )
 
 args = parser.parse_args()
